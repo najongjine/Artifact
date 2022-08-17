47e2e644123e2d9e0fe1b0250b71b4747f057117
najongjine <54340913+najongjine@users.noreply.github.com>
2022-08-17 17:37:16 +0900

    .

---
+++ Assets/Scripts/Bush Scripts/BushFruits.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BushFruits : MonoBehaviour
+{
+    [SerializeField]
+    private int[] amountPerType;
+
+    [SerializeField]
+    private float[] respawnTime;
+
+    private BushVisual bushVisual;
+
+    private bool hasFruits;
+    private float timer;
+
+    private void Start()
+    {
+
+        bushVisual = GetComponent<BushVisual>();
+
+        // randomly initialize some bushes and fruits
+        if (Random.Range(0, 2) == 0)
+        {
+            hasFruits = false;
+            timer = Time.time + respawnTime[(int)bushVisual.GetBushVariant()];
+        }
+        else
+        {
+            hasFruits = true;
+            bushVisual.ShowFruits();
+        }
+
+    }
+
+    private void Update()
+    {
+        if (Time.time > timer) {
+            hasFruits = true;
+            bushVisual.ShowFruits();
+        }
+    }
+
+    public int HarvestFruit() {
+
+        if (hasFruits)
+        {
+            hasFruits = false;
+            bushVisual.HideFruits();
+            timer = Time.time + respawnTime[(int)bushVisual.GetBushVariant()];
+            return amountPerType[(int)bushVisual.GetBushVariant()];
+        }
+        else
+            return 0;
+
+    }
+
+    public bool HasFruits() {
+        return hasFruits;
+    }
+
+    // when the enemy attacks the bush and eats it
+    public void EatBushFruits() {
+        enabled = false;
+        bushVisual.SetToDry();
+    }
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/Player Scripts/PlayerAnimation.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] animSprites;
+
+    public float timeTreshold = 0.1f;
+
+    private float timer;
+    private int state = 0;
+
+    private SpriteRenderer sr;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+
+        if (Time.time > timer)
+        {
+
+            //sr.sprite = animSprites[state % animSprites.Length];
+            //state++;
+
+            sr.sprite = animSprites[state];
+            state++;
+
+            if (state == animSprites.Length)
+                state = 0;
+
+            timer = Time.time + timeTreshold;
+
+        }
+
+    }
+
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
